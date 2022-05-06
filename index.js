6c417f0f074d86efd02d75f21874b2d88b84e681
twill <twill1@gmx.com>
2022-05-06 00:40:19 -0500

    Add mongoose for connecting to mongodb from node container, docker network basics

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+/*
+    We need to pass in some information for mongoose to connect to the mongodb container.
+    Instead of an IP we can use the service name (mongo) since composer created a custom
+    default network that has DNS. We can confirm a successful connection by checking 
+    the logs of the node container.
+*/
+mongoose.connect("mongodb://root:secret@mongo:27017/?authSource=admin")
+    .then(() => console.log("Connection Success"))
+    .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!</h2>")
